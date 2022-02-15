4f02c1e74dfd46a45b4449a3983bc0e0ec86fbdb
choheeseung <gmltmd0816@naver.com>
2022-02-15 18:07:01 +0900

    day7&8

--- Day 8/src/index.js
+++ Day 8/src/index.js
@@ -1,2 +1,49 @@
-const h1 = document.querySelector("h1");
-const h2 = document.querySelector("h2");
+const inputForm = document.getElementById("num-game");
+const finishNum = document.getElementById("finishNum");
+const GuessNum = document.querySelector("#GuessNum");
+const Result = document.querySelector(".result-message");
+const playbtn = document.querySelector("#play-btn");
+
+function getRandomNum(min, max) {
+    min = Math.ceil(min);
+    max = Math.ceil(max);
+    return parseInt(Math.ceil(Math.random() * (min + max) + min));
+}
+
+function NumberGame(event) {
+    event.preventDefault();
+    const FinishNum = parseInt(finishNum.value);
+    if (FinishNum < 0) {
+        alert("please write number over 0");
+        return;
+    } else {
+        localStorage.setItem("FinishNum", FinishNum);
+    }
+    const userNum = parseInt(GuessNum.value);
+    if (userNum < 0) {
+        alert("please write number over 0");
+    } else {
+        localStorage.setItem("userNum", userNum);
+    }
+    localStorage.setItem("machineNum", getRandomNum(0, localStorage.getItem("FinishNum"))
+    );
+}
+
+function PrintResult(event) {
+    event.preventDefault();
+
+    const userNum = localStorage.getItem("userNum");
+    const machineNum = localStorage.getItem("machineNum");
+    const UserMess = inputForm.querySelector(".number-message");
+    const ResultMess = inputForm.querySelector(".result-message");
+
+    if (machineNum === userNum) {
+        UserMess.innerHTML = `you choose: ${userNum}, the machine choose:${machineNum}`;
+        ResultMess.innerHTML = "You Won!";
+    } else {
+        UserMess.innerHTML = `you choose: ${userNum}, the machine choose:${machineNum}`;
+        ResultMess.innerHTML = "You Lost!";
+    }
+}
+playbtn.addEventListener("click", PrintResult);
+playbtn.addEventListener("click", NumberGame);
